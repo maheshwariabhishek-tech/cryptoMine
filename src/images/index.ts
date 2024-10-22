be62eccc66708428a7877b3210663b6d3135ec06
Abhishek Maheshwari <abhishekmaheshwari01234@gmail.com>
2024-10-22 15:10:44 +0530

    first commit

---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import hamsterExchange from "./hamster-exchange.png";
+import binanceLogo from "./hamster-exchange.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./hamster-exchange.png";
+import hamsterCoin from "./hamster-coin.png";
+
+export {
+    hamsterExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    hamsterCoin
+};
